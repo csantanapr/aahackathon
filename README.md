34026e0dd13c6a291f9c75207c8cc70f045126e0
Carlos Santana <csantana23@gmail.com>
2014-10-03 00:49:30 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bluemix and Cordova Demos for AA Hackathon 2014 (Oct 24-25)
+
+## How to do Mobile Data with Bluemix and Apache Cordova
+  [data-demo/README.md](data-demo/README.md)
---
+++ data-demo/bluelist-mobiledata-cordova/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,42 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.ibm.mobile.cordova.ibmbluemix/www/IBMBluemixHybrid.js",
+        "id": "com.ibm.mobile.cordova.ibmbluemix.hybrid",
+        "clobbers": [
+            "IBMBluemix.hybrid"
+        ]
+    },
+    {
+        "file": "plugins/com.ibm.mobile.cordova.ibmdata/www/IBMDataHybrid.js",
+        "id": "com.ibm.mobile.cordova.ibmdata.hybrid",
+        "clobbers": [
+            "IBMData.hybrid"
+        ]
+    },
+    {
+        "file": "plugins/com.ionic.keyboard/www/keyboard.js",
+        "id": "com.ionic.keyboard.keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.ibm.mobile.cordova.ibmbluemix": "1.0.0-20140927-1116",
+    "com.ibm.mobile.cordova.ibmdata": "1.0.0-20140927-1116",
+    "com.ionic.keyboard": "1.0.3",
+    "org.apache.cordova.console": "0.2.11",
+    "org.apache.cordova.device": "0.2.12"
+}
+// BOTTOM OF METADATA
+});
